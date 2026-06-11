8094c8d9e7e0ee68ad722e23268272fdd261f94c
purefier100 <benchristel5@gmail.com>
2026-06-11 18:05:20 +0100

    fix app link

--- backend/server.js
+++ backend/server.js
@@ -794,6 +794,8 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://127.0.0.1:3000",
   "http://localhost:5173",
+  "https://triviafi.xyz",
+  "https://www.triviafi.xyz",
   process.env.FRONTEND_URL,
 ].filter(Boolean);
 
